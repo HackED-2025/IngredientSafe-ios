a5a3fe6e1927e2629f6df4e1dced7ecf15bbcc83
Jacob Feng <peanutchowder@gmail.com>
2025-02-17 10:44:38 -0700

    updated styling to be consistent w theme

--- IngredientSafe/Views/FavoritesView.swift
+++ IngredientSafe/Views/FavoritesView.swift
@@ -2,34 +2,77 @@ import SwiftUI
 
 struct FavoritesView: View {
     @EnvironmentObject var favoritesModel: FavoritesModel
+    @Environment(\.presentationMode) var presentationMode
 
     var body: some View {
-        VStack {
-            Text("Your Favorite Products")
-                .font(.title)
-                .padding()
+        ZStack {
+            Theme.backgroundColor
+                .ignoresSafeArea()
 
-            List(favoritesModel.favoriteItems, id: \.self) { product in
-                HStack {
-                    Text(product.name)
-                    Spacer()
-                    Button(action: {
-                        favoritesModel.removeFavorite(product)
-                    }) {
-                        Image(systemName: "trash")
-                            .foregroundColor(.red)
-                    }
+            VStack(alignment: .leading) {
+
+                // Header
+                VStack(spacing: 8) {
+                    Image(systemName: "star.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 60, height: 60)
+                        .foregroundColor(Theme.accentGreen)
+
+                    Text("Favorites")
+                        .font(.system(size: 32, weight: .semibold))
+                        .foregroundColor(Theme.accentGreen)
                 }
-                .padding()
+
+                // Main List of Favorite Products
+                ScrollView {
+                    VStack(spacing: 12) {
+                        ForEach(favoritesModel.favoriteItems, id: \.self) { product in
+                            HStack {
+                                Text(product.name)
+                                    .foregroundColor(Theme.textColor)
+                                    .padding()
+                                Spacer()
+                                Button(action: {
+                                    favoritesModel.removeFavorite(product)
+                                }) {
+                                    Image(systemName: "trash")
+                                        .foregroundColor(.red)
+                                        .padding()
+                                        .background(Theme.textInputBgColor)
+                                        .clipShape(Circle())
+                                }
+                            }
+                            .padding()
+                            .background(Theme.textInputBgColor)
+                            .cornerRadius(10)
+                        }
+                    }
+                    .padding(.horizontal)
+                }
+
+                Spacer()
             }
+            .padding()
         }
-        .navigationTitle("Favorites")
+        .navigationBarTitle("", displayMode: .inline)
+        .navigationBarBackButtonHidden(true)
+        .navigationBarItems(leading: Button(action: {
+            presentationMode.wrappedValue.dismiss()
+        }) {
+            Image(systemName: "chevron.left")
+                .foregroundColor(Theme.accentGreen)
+            Text("Back")
+                .foregroundColor(Theme.accentGreen)
+        })
     }
 }
 
 struct FavoritesView_Previews: PreviewProvider {
     static var previews: some View {
-        FavoritesView()
-            .environmentObject(FavoritesModel())
+        NavigationStack {
+            FavoritesView()
+                .environmentObject(FavoritesModel())
+        }
     }
 }
